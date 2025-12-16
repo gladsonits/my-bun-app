4a5be78bd05d5ba7ac51255c02def6de9f51018f
gladsonits <gladson.bits@gmail.com>
2025-12-15 20:12:40 -0500

    Setup Prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Button } from "@/components/ui/button";
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import { Button } from '@/components/ui/button';
 
-import "./App.css";
+import './App.css';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.log(err));
-  }, []);
+   useEffect(() => {
+      fetch('api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message))
+         .catch((err) => console.log(err));
+   }, []);
 
-  return (
-    <div className="p-4">
-      <p className="font-bold text-3xl">{message}</p>
-      <Button>Click me</Button>
-    </div>
-  );
+   return (
+      <div className="p-4">
+         <p className="font-bold text-3xl">{message}</p>
+         <Button>Click me</Button>
+      </div>
+   );
 }
 
 export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,21 +1,21 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 const app = express();
 
 dotenv.config(); // Load environment variables from .env file
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+   res.send('Hello World!');
 });
 
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "Hello World!" });
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hello World!' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+   console.log(`Server is running on port http://localhost:${PORT}`);
 });
 export default app;
